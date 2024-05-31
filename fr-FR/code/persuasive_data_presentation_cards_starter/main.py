64f3a01de2c766f215ca0dcbca83aed7cb43b565
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-05-31 11:24:55 +0100

    New translations main.py (French)

--- fr-FR/code/persuasive_data_presentation_cards_starter/main.py
+++ fr-FR/code/persuasive_data_presentation_cards_starter/main.py
@@ -1,8 +1,8 @@
 #!/bin/python3
 
-# Data headings: 0 Number,	1 Name,	2 Type 1,	3 Type 2,	4 HP,	5 Att,	6 Def,	7 Spa,	8 Spd,	9 Spe,	10 BST,	11 Mean,	12 Standard Deviation
-# Data headings continued: 13 Generation,	14 Experience type,	15 Experience to level 100,	16 Final Evolution,	17 Catch Rate, 18	Legendary,	19 Mega Evolution
-# Data headings continued: 10 Alolan Form,	21 Galarian Form,	22 Against Normal,	23 Against Fire,	24 Against Water,	25 Against Electric
-# Data headings continued: 26 Against Grass,	27 Against Ice,	28 Against Fighting,	29 Against Poison,	30 Against Ground,	31 Against Flying,
-# Data headings continued: 32 Against Psychic,	33 Against Bug,	34 Against Rock,	35 Against Ghost,	36 Against Dragon,	37 Against Dark,	38 Against Steel
-# Data headings continued: 39 Against Fairy,	40 Height,	41 Weight,	42 BMI
+# En-têtes de données : 0 Nombre, 1 Nom, 2 Type 1, 3 Type 2, 4 HP, 5 Att, 6 Def, 7 Spa, 8 Spd, 9 Spe, 10 BST, 11 Moyenne, 12 Écart standard
+# En-têtes de données suite : 13 Génération, 14 Type d'expérience, 15 Expérience au niveau 100,16 Évolution finale, 17 Taux d'attraction, 18 Légendaire, 19 Mega Evolution
+# En-têtes de données suite : 10 Forme Alolan, 21 Forme Galarienne, 22 Contre la normale, 23 Contre le feu, 24 Contre l'eau, 25 Contre l'électricité
+# En-têtes de données suite : 26 Contre l'herbe, 27 Contre la glace, 28 Contre le combat, 29 Contre le poison, 30 Contre la terre, 31 Contre les vols,
+# En-têtes de données suite : 32 Contre Psychique, 33 Contre Bug, 34 Contre Roc, 35 Contre fantôme, 36 Contre Dragon, 37 Contre Sombre, 38 Contre Acier
+# En-têtes de données suite : 39 Contre Fée, 40 Taille, 41 Poids, 42 IMC
