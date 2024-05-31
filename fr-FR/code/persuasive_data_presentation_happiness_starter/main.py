bfae89ecf796735b156f92d2e1504ce29fa113a6
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-05-31 11:26:10 +0100

    New translations main.py (French)

--- fr-FR/code/persuasive_data_presentation_happiness_starter/main.py
+++ fr-FR/code/persuasive_data_presentation_happiness_starter/main.py
@@ -1,3 +1,3 @@
 #!/bin/python3
 
-# Data headings: 0 Overall rank,	1 Country or region,	2 Score,	3 GDP per capita, 4 Social support,	5 Healthy life expectancy,	6 Freedom to make life choices,	7 Generosity,	8 Perceptions of corruption
+# En-têtes de données : 0 Classement global, 1 Pays ou région, 2 Score, 3 PIB par habitant, 4 Soutien social, 5 Espérance de vie en bonne santé, 6 Liberté de faire des choix de vie, 7 Générosité, 8 Perceptions de la corruption
