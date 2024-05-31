974c64dc78c0df748d7a8d22417db028d8d0785a
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-05-31 11:29:42 +0100

    New translations main.py (French)

--- fr-FR/code/persuasive_data_presentation_ufo_example/main.py
+++ fr-FR/code/persuasive_data_presentation_ufo_example/main.py
@@ -1,126 +1,126 @@
 #!/bin/python3
 from p5 import *
 
-from xy import get_xy_coords
+from xy import obt_xy_coords
 
-# Draw the UFO on the map
-def draw_ufo(shape, x, y):
+# Dessiner l'OVNI sur la carte
+def dessine_ovni(forme, x, y):
 
-    global fireball, circle, tri, light, disk, misc, cylinder
-    fireball = Color(252, 186, 3)
-    circle = Color(32, 201, 49)
+    global boule_de_feu, cercle, tri, lumiere, disque, divers, cylindre
+    boule_de_feu = Color(252, 186, 3)
+    cercle = Color(32, 201, 49)
     tri = Color(241, 245, 32)
-    light = Color(247, 247, 245)
-    disk = Color(189, 189, 172)
-    misc = Color(255, 0, 0)
-    cylinder = Color(73, 99, 230)
+    lumiere = Color(247, 247, 245)
+    disque = Color(189, 189, 172)
+    divers = Color(255, 0, 0)
+    cylindre = Color(73, 99, 230)
 
-    if shape == 'fireball':
-        fill(fireball)
+    if forme == 'boule_de_feu':
+        fill(boule_de_feu)
         ellipse(x, y, 15, 10)
-    elif shape == 'circle':
-        fill(circle)
+    elif forme == 'cercle':
+        fill(cercle)
         ellipse(x, y, 8, 8)
-    elif shape == 'triangle':
+    elif forme == 'triangle':
         fill(tri)
         triangle(x-8, y-15, x, y, x+8, y-15)
-    elif shape == 'light':
-        fill(light)
+    elif forme == 'lumiere':
+        fill(lumiere)
         ellipse(x, y, 15, 15)
-    elif shape == 'disk':
-        fill(disk)
+    elif forme == 'disque':
+        fill(disque)
         ellipse(x, y, 20, 10)
-    elif shape == 'cylinder' or shape == 'cigar':
-        fill(cylinder)
+    elif forme == 'cylindre' or forme == 'cigare':
+        fill(cylindre)
         rect(x, y, 20, 10)
     else:
-        fill(misc)
+        fill(divers)
         ellipse(x, y, 10, 10)
 
 
-def preload():
-    global map
-    map = load_image('mercator.jpeg')
+def prechargement():
+    global carte
+    carte = load_image('mercator.jpeg')
 
 
 def setup():
 
     size(991, 768)
-    load_data('ufo-sightings.csv')
+    charge_donnees('ufo-sightings.csv')
     image(
-        map,  # The image to draw
-        0,  # The x of the top-left corner
-        0,  # The y of the top-left corner
-        width,  # The width of the image
-        height  # The height of the image
+        carte,  # L'image à dessiner
+        0,  # Le x du coin supérieur gauche
+        0,  # Le y du coin supérieur gauche
+        width,  # La largeur de l'image
+        height  # La hauteur de l'image
     )
-    draw_data()
+    dessine_donnees()
 
 
-def load_data(file_name):
+def charge_donnees(nom_fichier):
 
-    # Create a dictionary for each siting based on the data in the csv file
+    # Créer un dictionnaire pour chaque site basé sur les données du fichier csv
 
-    global ufo_sightings
+    global observations_ovni
 
-    ufo_sightings = []
+    observations_ovni = []
 
-    with open(file_name) as f:
-        for line in f:
-            info = line.split(',')
-            ufo_dict = {
+    with open(nom_fichier) as f:
+        for ligne in f:
+            info = ligne.split(',')
+            dict_ovni = {
                 'date': info[0],
-                'time': info[1],
-                'state': info[2],
-                'country': info[3],
-                'shape': info[4],
-                'duration': info[5],
+                'heure': info[1],
+                'état': info[2],
+                'pays': info[3],
+                'forme': info[4],
+                'durée': info[5],
                 'latitude': info[6],
                 'longitude': info[7]
             }
-            ufo_sightings.append(ufo_dict)  # Store dictionary in a list
+            observations_ovni.append(dict_ovni) # Stocker le dictionnaire dans une liste
 
 
-def draw_data():
+def dessine_donnees():
 
-    # Use the lat and long data to calculate the x y coords for the shape
+    # Utilise les données de latitude et de longueur pour calculer les coordonnées x y de la forme
 
-    for sighting in ufo_sightings:
+    for observation in observations_ovni:
 
-        longitude = float(sighting['longitude'])
-        latitude = float(sighting['latitude'])
+        longitude = float(observation['longitude'])
+        latitude = float(observation['latitude'])
 
-        region_coords = get_xy_coords(longitude, latitude)
+        region_coords = obt_xy_coords(longitude, latitude)
 
-        region_x = region_coords['x']
-        region_y = region_coords['y']
+        region_x = coords_region['x']
+        region_y = coords_region['y']
 
-        shape = sighting['shape']
+        forme = observation['forme']
 
-        draw_ufo(shape, region_x, region_y)
+        dessine_ovni(formes, region_x, region_y)
 
 
 def mouse_pressed():
 
-    # Display a message depending on what shape the user has pressed
+    # Affiche un message en fonction de la forme sur laquelle l'utilisateur a appuyé
 
-    pixel_colour = Color(get(mouse_x, mouse_y)).hex
-    if pixel_colour == fireball.hex:
-        print('A fireball UFO was spotted here!')
-    elif pixel_colour == circle.hex:
-        print('A circle shaped UFO was spotted here!')
-    elif pixel_colour == tri.hex:
-        print('A triangle shaped UFO was spotted here!')
-    elif pixel_colour == light.hex:
-        print('A UFO made of light was spotted here!')
-    elif pixel_colour == disk.hex:
-        print('A disk shaped UFO was spotted here!')
-    elif pixel_colour == misc.hex:
-        print('A random shaped UFO was spotted here!')
-    elif pixel_colour == cylinder.hex:
-        print('A cylinder shaped UFO was spotted here!')
+    couleur_pixel = Color(get(mouse_x, mouse_y)).hex
+    if couleur_pixel == boule_de_feu.hex:
+        print('Un OVNI boule de feu a été repéré ici !')
+    if couleur_pixel == cercle.hex:
+        print('Un OVNI en forme de cercle a été repéré ici !')
+    if couleur_pixel == tri.hex:
+        print('Un OVNI en forme de triangle a été repéré ici !')
+    if couleur_pixel == lumiere.hex:
+        print('Un OVNI fait de lumière a été repéré ici !')
+    if couleur_pixel == disque.hex:
+        print('Un OVNI en forme de disque a été repéré ici !')
+    if couleur_pixel == divers.hex:
+        print('Un OVNI de forme aléatoire a été repéré ici !')
+    if couleur_pixel == cylindre.hex:
+        print('Un OVNI en forme de cylindre a été repéré ici !')
     else:
-        print('There were no UFO sightings in this area!')
+        print('Il n'y a eu aucune observation d'OVNI dans cette zone !')
 
 
 run()
