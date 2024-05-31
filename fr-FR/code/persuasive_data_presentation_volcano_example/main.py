f95d5e568a4c1e1d269fb8893fcd1b6f68b6f977
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-05-31 11:31:03 +0100

    New translations main.py (French)

--- fr-FR/code/persuasive_data_presentation_volcano_example/main.py
+++ fr-FR/code/persuasive_data_presentation_volcano_example/main.py
@@ -2,84 +2,84 @@
 from p5 import *
 from random import randint
 
-# A visualisation of volcano erruptions since 2010
+# Une visualisation des éruptions volcaniques depuis 2010
 
-# CSV Headings: 0 Year, 1 Month, 2 Day, 3 Name, 4 Location, 5 Country, 6 Latitude, 7 Longitude, 8 Elevation, 9 Type, 10 Status
+# En-têtes CSV : 0 Année, 1 Mois, 2 Jour, 3 Nom, 4 Emplacement, 5 Pays, 6 Latitude, 7 Longitude, 8 Altitude, 9 Type, 10 Statut
 
-from xy import get_xy_coords
+from xy import obt_xy_coords
 
-colours = {}
+couleurs = {}
 
-# Draw the volcano on the map
-def draw_volcano(colour, x, y):
-    fill(colour)
+# Dessiner le volcan sur la carte
+def dessiner_volcan(couleur, x, y):
+    fill(couleur)
     ellipse(x, y, 12, 12)
 
 
-def preload():
-    global map
-    map = load_image('mercator.jpeg')
+def prechargement():
+    global carte
+    carte = load_image('mercator.jpeg')
 
 
 def setup():
     size(991, 768)
     image(
-        map,  # The image to draw
-        0,  # The x of the top-left corner
-        0,  # The y of the top-left corner
-        width,  # The width of the image
-        height  # The height of the image
+        carte,  # L'image à dessiner
+        0,  # Le x du coin supérieur gauche
+        0,  # Le y du coin supérieur gauche
+        width,  # La largeur de l'image
+        height  # La hauteur de l'image
     )
-    load_data('volcano-data.csv')
-    draw_data()
+    charge_donnees('volcano-data.csv')
+    dessine_donnees()
 
 
-def load_data(file_name):
-    # Create a dictionary for each siting based on the data in the csv file
+def charge_donnees(nom_fichier):
+    # Créer un dictionnaire pour chaque site basé sur les données du fichier csv
 
-    global volcano_eruptions
+    global eruptions_volcan
 
-    volcano_eruptions = []
+    eruptions_volcan = []
 
-    with open(file_name) as f:
-        for line in f:
-            info = line.split(',')
-            volcano_dict = {
+    with open(nom_fichier) as f:
+        for ligne in f:
+            info = ligne.split(',')
+            dict_volcan = {
                 'longitude': info[7],
                 'latitude': info[6],
-                'year': info[0],
-                'region': info[5]
+                'année': info[0],
+                'région': info[5]
             }
-            # Store dictionary in a list
-            volcano_eruptions.append(volcano_dict)
+            # Stocker le dictionnaire dans une liste
+            eruptions_volcan.append(volcan_dict)
 
 
-def draw_data():
+def dessine_donnees():
     no_stroke()
 
-    blue_value = 0
-    # Use the lat and long data to calculate the x y coords for the shape
-    for eruption in volcano_eruptions:
+    valeur_bleu = 0
+    # Utilise les données de latitude et de longueur pour calculer les coordonnées x y de la forme
+    for eruption in eruptions_volcan:
         longitude = float(eruption['longitude'])
         latitude = float(eruption['latitude'])
-        region_coords = get_xy_coords(longitude, latitude)
-        region_x = region_coords['x']
-        region_y = region_coords['y']
-        colour = Color(255, 100, blue_value)  # Select a random colour
-        colours[colour.hex] = eruption
-        draw_volcano(colour, region_x, region_y)
-        blue_value += 1
+        region_coords = obt_xy_coords(longitude, latitude)
+        region_x = coords_region['x']
+        region_y = coords_region['y']
+        couleur = Color(255, 100, blue_value) # Sélectionne une couleur aléatoire
+        couleurs[couleur.hex] = eruption
+        dessiner_volcan(couleur, region_x, region_y)
+        valeur_bleu -= 1
 
 
 def mouse_pressed():
-    # Put code to run when the mouse is pressed here
-    pixel_colour = Color(get(mouse_x, mouse_y)).hex
-    if pixel_colour in colours:
-        facts = colours[pixel_colour]
-        print('A volcano erupted in ' +
-              facts['region'] + ' in ' + facts['year'])
+    # Place ici le code à exécuter lorsque la souris est cliquée
+    couleur_pixel = Color(get(mouse_x, mouse_y)).hex
+    if couleur_pixel in couleurs:
+        faits = couleurs[couleur_pixel]
+        print('Un volcan est entré en éruption en' +
+              faits['région'] + 'dans ' + faits['année'])
     else:
-        print('Region not detected')
+        print('Région non détectée')
 
 
 run()
