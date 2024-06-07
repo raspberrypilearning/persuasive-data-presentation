8e0ad5922da5b686a8f64ac8635028edec5f1466
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-06-07 11:26:55 +0100

    New translations main.py (Dutch)

--- nl-NL/code/persuasive_data_presentation_ufo_starter/main.py
+++ nl-NL/code/persuasive_data_presentation_ufo_starter/main.py
@@ -1,3 +1,3 @@
 #!/bin/python3
 
-# CSV Headings: 0 Year, 1 Month, 2 Day, 3 Name, 4 Location, 5 Country, 6 Latitude, 7 Longitude, 8 Elevation, 9 Type, 10 Status
+# CSV labels: 0 Jaar, 1 Maand, 2 Dag, 3 Naam, 4 Locatie, 5 Land, 6 Breedtegraad, 7 Lengtegraad, 8 Hoogte, 9 Type, 10 Status
