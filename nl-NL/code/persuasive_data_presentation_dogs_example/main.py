a5350bbb52e2768a2d12eca71f75988cdc464d3b
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-06-07 10:12:14 +0100

    New translations main.py (Dutch)

--- nl-NL/code/persuasive_data_presentation_dogs_example/main.py
+++ nl-NL/code/persuasive_data_presentation_dogs_example/main.py
@@ -1,61 +1,61 @@
 #!/bin/python3
 
-# Data headings: 0 BreedName, 1 Type,	2 Average Price (American Dollars),	3 Intelligence score,	4 Popularity score US 2017
+# Gegevens rubrieken: 0 Rasnaam, 1 Type, 2 Gemiddelde prijs (Amerikaanse dollars), 3 Intelligentiescore, 4 Populariteitsscore VS 2017
 
 from pygal import *
 
-with open('dog_breed_characteristics.csv') as f:
+with open('dog_breed_characteristics.csv') als f:
     data = f.read()
     lines = data.splitlines()
 
-choice = 0
+keuze = 0
 
 
 def main():
 
-    choice = input(
-        'Analysing Toy dogs. What would you like to see? \n1. Average price, \n2. Popularity, \n3. Intelligence, \nChoice:')
+    keuze = input(
+        'Analyseren van kleine honden. Wat zou je willen zien? \n1. Gemiddelde prijs, \n2. Populariteit, \n3. Intelligentie, \nKeuze:')
 
-    if choice == '1':
+    if keuze == '1':
         chart = Bar(width=600, height=400,
-                    title=' 🐶 Average price of Toy dogs by breed 🐶 ')
+                    title=' 🐶 Gemiddelde prijs van kleine honden per ras 🐶 ')
         for line in lines:
             info = line.split(',')
-            BreedName = info[0]
+            Rasnaam = info[0]
             Type = info[1]
-            AvgPrice = info[2]
-            Intelligence = info[3]
-            Popularity = info[4]
+            GemPrijs = info[2]
+            Intelligentie = info[3]
+            Populariteit = info[4]
             if Type == 'Toy':
-                chart.add(BreedName, float(AvgPrice))
+                chart.add(rasnaam, float(gemprijs))
         chart.render()
 
-    if choice == '2':
+    if keuze == '2':
         chart = Pie(width=600, height=400,
-                    title='🐶 Popularity of Toy dogs by breed 🐶 ')
+                    title=' 🐶 Populariteit van kleine honden per ras 🐶 ')
         for line in lines:
             info = line.split(',')
-            BreedName = info[0]
+            RasNaam = info[0]
             Type = info[1]
-            AvgPrice = info[2]
-            Intelligence = info[3]
-            Popularity = info[4]
+            GemPrijs = info[2]
+            Intelligentie = info[3]
+            Populariteit = info[4]
             if Type == 'Toy':
-                chart.add(BreedName, float(Popularity))
+                chart.add(RasNaam, float(populariteit))
         chart.render()
 
-    if choice == '3':
+    if keuze == '3':
         chart = Bar(width=600, height=400,
-                    title='🐶 Intelligence of Toy dogs by breed 🐶')
+                    title='🐶 Intelligentie van kleine honden per ras 🐶')
         for line in lines:
             info = line.split(',')
-            BreedName = info[0]
+            RasNaam = info[0]
             Type = info[1]
-            AvgPrice = info[2]
-            Intelligence = info[3]
-            Popularity = info[4]
+            GemPrijs = info[2]
+            Intelligentie = info[3]
+            Populariteit = info[4]
             if Type == 'Toy':
-                chart.add(BreedName, float(Intelligence))
+                chart.add(RasNaam, float(intelligentie))
         chart.render()
     main()
 
