fff521bd81f8e1a7f2f8c8cff06d123669eb5c2f
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-06-07 10:12:11 +0100

    New translations main.py (Dutch)

--- nl-NL/code/persuasive_data_presentation_cards_starter/main.py
+++ nl-NL/code/persuasive_data_presentation_cards_starter/main.py
@@ -1,8 +1,8 @@
 #!/bin/python3
 
-# Data headings: 0 Number,	1 Name,	2 Type 1,	3 Type 2,	4 HP,	5 Att,	6 Def,	7 Spa,	8 Spd,	9 Spe,	10 BST,	11 Mean,	12 Standard Deviation
-# Data headings continued: 13 Generation,	14 Experience type,	15 Experience to level 100,	16 Final Evolution,	17 Catch Rate, 18	Legendary,	19 Mega Evolution
-# Data headings continued: 10 Alolan Form,	21 Galarian Form,	22 Against Normal,	23 Against Fire,	24 Against Water,	25 Against Electric
-# Data headings continued: 26 Against Grass,	27 Against Ice,	28 Against Fighting,	29 Against Poison,	30 Against Ground,	31 Against Flying,
-# Data headings continued: 32 Against Psychic,	33 Against Bug,	34 Against Rock,	35 Against Ghost,	36 Against Dragon,	37 Against Dark,	38 Against Steel
-# Data headings continued: 39 Against Fairy,	40 Height,	41 Weight,	42 BMI
+# Gegevens rubrieken: 0 Aantal, 1 Naam, 2 Type 1, 3 Type 2, 4 HP, 5 Att, 6 Def, 7 Spa, 8 Spd, 9 Spe, 10 BST, 11 Gemiddelde, 12 Standaardafwijking
+# Gegevens rubrieken vervolg: 13 Generatie, 14 Ervaringstype, 15 Ervaring tot niveau 100, 16 Laatste evolutie, 17 Catch Rate, 18 Legendarisch, 19 Mega-evolutie
+# Gegevensrubrieken vervolg: 10 Alolan-vorm, 21 Galarische vorm, 22 Tegen normaal, 23 Tegen vuur, 24 Tegen water, 25 Tegen elektrisch
+# Gegevens rubrieken vervolg: 26 Tegen gras, 27 Tegen ijs, 28 Tegen vechten, 29 Tegen gif, 30 Tegen grond, 31 Tegen vliegen,
+# Gegevensrubrieken vervolg: 32 Tegen Psychic, 33 Tegen Bug, 34 Tegen Rots, 35 Tegen Geest, 36 Tegen Draak, 37 Tegen Donker, 38 Tegen Staal
+# Gegevens rubrieken vervolg: 39 Tegen Fee, 40 Lengte, 41 Gewicht, 42 BMI
