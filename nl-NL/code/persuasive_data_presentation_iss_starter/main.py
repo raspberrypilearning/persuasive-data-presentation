8f419df98719e04a652a2a6f878ed64570e92051
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-06-07 11:26:48 +0100

    New translations main.py (Dutch)

--- nl-NL/code/persuasive_data_presentation_iss_starter/main.py
+++ nl-NL/code/persuasive_data_presentation_iss_starter/main.py
@@ -1,3 +1,3 @@
 #!/bin/python3
 
-# Data headings: 0 Expedition number, 1 Representing country, 2 Astronaut, 3 Mission launch date
+# Gegevens rubrieken: 0 Expeditienummer, 1 Vertegenwoordigend land, 2 Astronaut, 3 Lanceringsdatum missie
